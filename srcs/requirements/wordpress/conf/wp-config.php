d45690610dcbe21b730a55605540c847da4ffdea
jpfannku <jpfannku@student.42wolfsburg.de>
2023-04-07 14:16:34 +0200

    initial commit

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,17 @@
+define('DB_NAME', '$MARIADB_DATABASE');
+define('DB_USER', '$MARIADB_USER');
+define('DB_PASSWORD', '$MARIADB_PASSWORD');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',		'$AUTH_KEY');
+define('SECURE_AUTH_KEY',	'$SECURE_AUTH_KEY');
+define('LOGGED_IN_KEY',		'$LOGGED_IN_KEY');
+define('NONCE_KEY',		'$NONCE_KEY');
+define('AUTH_SALT',		'$AUTH_SALT);
+define('SECURE_AUTH_SALT',	'$SECURE_AUTH_SALT');
+define('LOGGED_IN_SALT',	'$LOGGED_IN_SALT');
+define('NONCE_SALT',		'$NONCE_SALT');
+
+$table_prefix  = '$WORDPRESS_TABLE_PREFIX';
